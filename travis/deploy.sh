cada86e0d9496f38ba7b48f30cc3744146866f13
Jocelyn Le Sage <jocelyn@le-sage.com>
2017-03-03 08:51:10 -0500

    Changed Travis deployment: Push generated Dockerfile to a branch matching the docker file. This will allow using Docker automated builds.

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,6 +3,28 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
-echo "Pushing docker image to $DOCKER_REPO:$TAG..."
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" 
-docker push $DOCKER_REPO:$TAG
+TARGET_BRANCH=$TAG
+
+# Switch to proper branch and sync it with master.
+git branch | grep -w -q $TARGET_BRANCH || git branch $TARGET_BRANCH
+git checkout $TARGET_BRANCH
+git rebase master
+
+# Generate and validate the Dockerfile.
+echo "Generating and validating the Dockerfile..."
+travis/before_script.sh
+
+echo "The following change will be pushed to branch $TARGET_BRANCH:"
+git diff -u Dockerfile
+
+# Add and commit the Dockerfile.
+git add Dockerfile
+git commit
+    -m "Automatic Dockerfile deployment from Travis CI (build $TRAVIS_BUILD_NUMBER)."
+    --author="Travis CI <$COMMIT_AUTHOR_EMAIL>"
+
+# Push the change.
+echo "Pushing change to repository..."
+REPO=$(git config remote.origin.url)
+REPO=${REPO/https:\/\//https:\/\/$GIT_PERSONAL_ACCESS_TOKEN@}
+git push $REPO $TAG
