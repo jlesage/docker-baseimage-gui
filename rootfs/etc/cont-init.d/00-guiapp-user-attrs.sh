4b30074b3ebf4a0b3de838b87e9e77cd66e7a4d2
Jocelyn Le Sage <jocelyn@le-sage.com>
2017-07-08 21:01:17 -0400

    Use the $APP_USER variable.  No change in functionality.

--- rootfs/etc/cont-init.d/00-guiapp-user-attrs.sh
+++ rootfs/etc/cont-init.d/00-guiapp-user-attrs.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/with-contenv sh
 
 #
-# Adjust ownership of files and directories owned by the guiapp user.
+# Adjust ownership of files and directories owned by the app user.
 #
 
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
-chown -R $USER_ID:$GROUP_ID /home/guiapp
+chown -R $USER_ID:$GROUP_ID /home/$APP_USER
 chown -R $USER_ID:$GROUP_ID /config
 
 # vim: set ft=sh :
--- rootfs/etc/cont-init.d/00-guiapp-user-map.sh
+++ rootfs/etc/cont-init.d/00-guiapp-user-map.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/with-contenv sh
 
 #
-# Add the 'guiapp' user to the password and group databases.  This is needed
-# just to make sure that mapping between the user/group ID and its name is
-# possible.
+# Add the app user to the password and group databases.  This is needed just to
+# make sure that mapping between the user/group ID and its name is possible.
 #
 
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
-echo "guiapp:x:$USER_ID:$GROUP_ID::/home/guiapp:/sbin/nologin" >> /etc/passwd
-echo "guiapp:x:$GROUP_ID:" >> /etc/group
+echo "$APP_USER:x:$USER_ID:$GROUP_ID::/home/$APP_USER:/sbin/nologin" >> /etc/passwd
+echo "$APP_USER:x:$GROUP_ID:" >> /etc/group
 
 # vim: set ft=sh :
