746d528257b24d5719f6dc7de7cb7c30f9c3c39a
Jocelyn Le Sage <jocelyn@le-sage.com>
2017-10-08 20:44:47 -0400

    Fixed nginx config for systems without IPv6 support.

--- rootfs/etc/cont-init.d/10-nginx.sh
+++ rootfs/etc/cont-init.d/10-nginx.sh
@@ -11,6 +11,9 @@ if [ "${SECURE_CONNECTION:-0}" -eq 0 ]; then
     sed-patch '/^[\t]ssl_dhparam/d' /etc/nginx/default_site.conf
     sed-patch 's/:5950;/:5900;/' /etc/nginx/default_site.conf
 fi
+if ! ifconfig -a | grep -wq inet6; then
+    sed-patch '/^[\t]listen \[::\]:5800 ssl default_server;/d' /etc/nginx/default_site.conf
+fi
 
 # Make sure required directories exist.
 s6-setuidgid $USER_ID:$GROUP_ID mkdir -p /config/log/nginx
