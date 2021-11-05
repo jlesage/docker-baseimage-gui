1bc0f7cb3c8dcc7e9c7a0b620b7d041712d876de
Jocelyn Le Sage <jocelyn@le-sage.com>
2021-11-04 20:03:42 -0400

    Make sure the generated file has the correct (read) permissions.

--- rootfs/etc/cont-init.d/10-web-index.sh
+++ rootfs/etc/cont-init.d/10-web-index.sh
@@ -7,4 +7,7 @@ set -u # Treat unset variables as an error.
 cp /opt/novnc/index.vnc /opt/novnc/index.html
 sed-patch "s/\$DESKTOP/$APP_NAME/g" /opt/novnc/index.html
 
+# Make sure the file has the right permissions.
+chmod 644 /opt/novnc/index.html
+
 # vim:ft=sh:ts=4:sw=4:et:sts=4
