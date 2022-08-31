ae0d94193bdb500e0754ea9c76b5580c0a6cd406
Benedict Chan <purple.enchanted@gmail.com>
2022-08-31 15:39:49 +1000

    Add RSA signing keys for untrusted installation of CJK font, fix #39

--- rootfs/etc/cont-init.d/10-cjk-font.sh
+++ rootfs/etc/cont-init.d/10-cjk-font.sh
@@ -10,6 +10,7 @@ if [ "${ENABLE_CJK_FONT:-0}" -eq 1 ]; then
     else
         echo "installing CJK font..."
         if [ -n "$(which apk)" ]; then
+            apk add -X https://dl-cdn.alpinelinux.org/alpine/v3.16/main -u alpine-keys
             add-pkg wqy-zenhei --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing 2>&1
         else
             add-pkg fonts-wqy-zenhei 2>&1
