16e1b434f7721bfff3cbcd5feac355c18a27ec69
Jocelyn Le Sage <jocelyn@le-sage.com>
2022-10-14 07:23:16 -0400

    Use `printf` instead of `echo` to have consistent result across different shells.

--- rootfs/etc/cont-init.d/10-web-data.sh
+++ rootfs/etc/cont-init.d/10-web-data.sh
@@ -8,31 +8,30 @@ WEB_DATA_FILE="/tmp/.web_data.json"
 rm -f "$WEB_DATA_FILE"
 
 # Beginning of the JSON file.
-echo "{" >> "$WEB_DATA_FILE"
+printf '{\n' >> "$WEB_DATA_FILE"
 
 # Add application name.
-echo -ne "    \"applicationName\": \"$APP_NAME\"" >> "$WEB_DATA_FILE"
+printf '    "applicationName": "%s"' "$APP_NAME" >> "$WEB_DATA_FILE"
 
 # Add application version.
 if [ -n "${APP_VERSION:-}" ]; then
-    echo -ne ",\n    \"applicationVersion\": \"$APP_VERSION\"" >> "$WEB_DATA_FILE"
+    printf ',\n    "applicationVersion": "%s"' "$APP_VERSION" >> "$WEB_DATA_FILE"
 fi
 
 # Add Docker image version.
 if [ -n "${DOCKER_IMAGE_VERSION:-}" ]; then
-    echo -ne ",\n    \"dockerImageVersion\": \"$DOCKER_IMAGE_VERSION\"" >> "$WEB_DATA_FILE"
+    printf ',\n    "dockerImageVersion": "%s"' "$DOCKER_IMAGE_VERSION" >> "$WEB_DATA_FILE"
 fi
 
 # Add dark mode.
 if [ "${DARK_MODE:-0}" -eq 1 ]; then
-    echo -ne ",\n    \"darkMode\": true" >> "$WEB_DATA_FILE"
+    printf ',\n    "darkMode": true' >> "$WEB_DATA_FILE"
 else
-    echo -ne ",\n    \"darkMode\": false" >> "$WEB_DATA_FILE"
+    printf ',\n    "darkMode": false' >> "$WEB_DATA_FILE"
 fi
 
 # End of the JSON file.
-echo -ne "\n" >> "$WEB_DATA_FILE"
-echo "}" >> "$WEB_DATA_FILE"
+printf '\n}\n' >> "$WEB_DATA_FILE"
 
 # Make sure the file has the right permissions.
 chmod 444 "$WEB_DATA_FILE"
