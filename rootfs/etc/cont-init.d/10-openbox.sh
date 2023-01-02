efbf8a5fb00bdfa59219f0557268d3fb48c1f66d
Jocelyn Le Sage <jocelyn@le-sage.com>
2023-01-01 20:43:13 -0500

    - Adjusted how the Openbox config is generated. - Added Openbox config for YAD.

--- rootfs/etc/cont-init.d/10-openbox.sh
+++ rootfs/etc/cont-init.d/10-openbox.sh
@@ -17,10 +17,9 @@ else
 fi
 
 #
-# Setup selection of the main window.
+# Setup selection criterias of the main window.
 #
 
-APP_DEF_FILE="$(mktemp)"
 APP_DEF_NAME=
 APP_DEF_CLASS=
 APP_DEF_GROUP_NAME=
@@ -61,36 +60,31 @@ else
     APP_DEF_TYPE=normal
 fi
 
-# Generate the application definitions block.
-echo "  <application" >> "$APP_DEF_FILE"
+# Generate matching criterias.
+CRITERIAS=
 if [ -n "$APP_DEF_NAME" ]; then
-    echo "    name=\"$APP_DEF_NAME\"" >> "$APP_DEF_FILE"
+    CRITERIAS="$CRITERIAS name=\"$APP_DEF_NAME\""
 fi
 if [ -n "$APP_DEF_CLASS" ]; then
-    echo "    class=\"$APP_DEF_CLASS\"" >> "$APP_DEF_FILE"
+    CRITERIAS="$CRITERIAS class=\"$APP_DEF_CLASS\""
 fi
 if [ -n "$APP_DEF_GROUP_NAME" ]; then
-    echo "    groupname=\"$APP_DEF_GROUP_NAME\"" >> "$APP_DEF_FILE"
+    CRITERIAS="$CRITERIAS groupname=\"$APP_DEF_GROUP_NAME\""
 fi
 if [ -n "$APP_DEF_GROUP_CLASS" ]; then
-    echo "    groupclass=\"$APP_DEF_GROUP_CLASS\"" >> "$APP_DEF_FILE"
+    CRITERIAS="$CRITERIAS groupclass=\"$APP_DEF_GROUP_CLASS\""
 fi
 if [ -n "$APP_DEF_ROLE" ]; then
-    echo "    role=\"$APP_DEF_ROLE\"" >> "$APP_DEF_FILE"
+    CRITERIAS="$CRITERIAS role=\"$APP_DEF_ROLE\""
 fi
 if [ -n "$APP_DEF_TITLE" ]; then
-    echo "    title=\"$APP_DEF_TITLE\"" >> "$APP_DEF_FILE"
+    CRITERIAS="$CRITERIAS title=\"$APP_DEF_TITLE\""
 fi
 if [ -n "$APP_DEF_TYPE" ]; then
-    echo "    type=\"$APP_DEF_TYPE\"" >> "$APP_DEF_FILE"
+    CRITERIAS="$CRITERIAS type=\"$APP_DEF_TYPE\""
 fi
-echo "  >" >> "$APP_DEF_FILE"
-echo "    <decor>no</decor>" >> "$APP_DEF_FILE"
-echo "    <maximized>true</maximized>" >> "$APP_DEF_FILE"
-echo "    <layer>below</layer>" >> "$APP_DEF_FILE"
-echo "  </application>" >> "$APP_DEF_FILE"
 
 # Write the final Openbox config file.
-cat /opt/base/etc/openbox/rc.xml.template | sed -e '/MAIN_APP_WINDOW_DEF/ {' -e "r $APP_DEF_FILE" -e 'd' -e '}' > /var/run/openbox/rc.xml
+cat /opt/base/etc/openbox/rc.xml.template | sed "s/%MAIN_APP_WINDOW_MATCH_CRITERIAS%/$CRITERIAS/" > /var/run/openbox/rc.xml
 
 # vim:ft=sh:ts=4:sw=4:et:sts=4
