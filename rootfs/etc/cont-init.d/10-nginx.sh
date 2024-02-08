ab576b24c75ecce1fbc5a09d00f0f58dd958b681
Jocelyn Le Sage <jocelyn@le-sage.com>
2024-02-08 18:10:35 -0500

    Adjusted nginx configuration to avoid patching files.

--- rootfs/etc/cont-init.d/10-nginx.sh
+++ rootfs/etc/cont-init.d/10-nginx.sh
@@ -12,50 +12,60 @@ NGINX_DIRS="\
     /var/run/nginx \
     /var/tmp/nginx \
 "
-DEFAULT_SITE_CONF=/var/tmp/nginx/default_site.conf
-DEFAULT_STREAM_CONF=/var/tmp/nginx/default_stream.conf
+LISTEN_CONF=/var/tmp/nginx/listen.conf
+SSL_CONF=/var/tmp/nginx/ssl.conf
+STREAM_CONF=/var/tmp/nginx/stream.conf
+STREAM_LISTEN_CONF=/var/tmp/nginx/stream_listen.conf
 
 # Make sure required directories exist.
 for DIR in $NGINX_DIRS; do
     mkdir -p "$DIR"
 done
 
-# First, restore default configuration files.
-cp /defaults/default_site.conf "$DEFAULT_SITE_CONF"
-rm -f "$DEFAULT_STREAM_CONF"
+# First, clear all dynamic config files.
+rm -f /var/tmp/nginx/*.conf
 
-# Adjust SSL related configuration.
-if is-bool-val-false "${SECURE_CONNECTION:-0}"; then
-    # Secure connection disabled: remove ssl related setting from the site
-    # config.
-    sed-patch 's/ssl default_server/default_server/g' "$DEFAULT_SITE_CONF"
-    sed-patch '/^[\t]ssl_certificate/d' "$DEFAULT_SITE_CONF"
-    sed-patch '/^[\t]ssl_dhparam/d' "$DEFAULT_SITE_CONF"
-elif [ "${SECURE_CONNECTION_VNC_METHOD:-SSL}" = "SSL" ]; then
-    # SSL secure connection enabled: activate the default stream config.
-    if [ "${VNC_LISTENING_PORT:-5900}" -ne -1 ]; then
-        cp /defaults/default_stream.conf "$DEFAULT_STREAM_CONF"
-        if [ "${VNC_LISTENING_PORT:-5900}" -ne 5900 ]; then
-            sed-patch "s/5900/$VNC_LISTENING_PORT/g" "$DEFAULT_STREAM_CONF"
-        fi
+# Generate the listen directive for HTTP access.
+if true; then
+    # Determine the listen port.
+    if [ "${WEB_LISTENING_PORT:-5800}" -eq -1 ]; then
+        # Port disabled: listen only on a unix socket to make nginx happy.
+        LISTEN_PORT="unix:/var/run/nginx/nginx.sock"
+    else
+        LISTEN_PORT="${WEB_LISTENING_PORT:-5800}"
+    fi
+
+    # Determine if secure port is used.
+    if is-bool-val-true "${SECURE_CONNECTION:-0}"; then
+        LISTEN_SSL="ssl"
+    else
+        LISTEN_SSL=
+    fi
+
+    # Add the listen directive.
+    echo "listen $LISTEN_PORT $LISTEN_SSL default_server;" >> "$LISTEN_CONF"
+
+    # Add the listen directive for IPv6.
+    if [ "${WEB_LISTENING_PORT:-5800}" -ne -1 ] && ifconfig -a | grep -wq inet6; then
+        echo "listen [::]:$LISTEN_PORT $LISTEN_SSL default_server;" >> "$LISTEN_CONF"
     fi
 fi
 
-# Adust listening port.
-if [ "${WEB_LISTENING_PORT:-5800}" -eq -1 ]; then
-    # Port disabled: listen only on a unix socket to make nginx happy.
-    sed-patch '/^[\t]listen \[::\]:5800 /d' "$DEFAULT_SITE_CONF"
-    sed-patch 's|5800|unix:/var/run/nginx/nginx.sock|g' "$DEFAULT_SITE_CONF"
-else
-    # Disable IPv6 listening if not supported.
-    if ! ifconfig -a | grep -wq inet6; then
-        sed-patch '/^[\t]listen \[::\]:5800 /d' "$DEFAULT_SITE_CONF"
-    fi
+# Handle SSL configuration.
+if is-bool-val-true "${SECURE_CONNECTION:-0}"; then
+    cp /defaults/default_ssl.conf "$SSL_CONF"
+fi
 
-    # Set the configured listening port.
-    if [ "${WEB_LISTENING_PORT:-5800}" -ne 5800 ]; then
-        sed-patch "s/5800/$WEB_LISTENING_PORT/g" "$DEFAULT_SITE_CONF"
-    fi
+# Handle stream configuration.
+# The stream config is needed only when secure connection is enabled, with the
+# VNC method set to SSL.
+if is-bool-val-true "${SECURE_CONNECTION:-0}" && [ "${SECURE_CONNECTION_VNC_METHOD:-SSL}" = "SSL" ] && [ "${VNC_LISTENING_PORT:-5900}" -ne -1 ]
+then
+    # Copy the default config.
+    cp /defaults/default_stream.conf "$STREAM_CONF"
+
+    # Generate the listen directive for stream config.
+    echo "listen ${VNC_LISTENING_PORT:-5900} ssl;" >> "$STREAM_LISTEN_CONF"
 fi
 
 # Make sure required directories are properly owned.
