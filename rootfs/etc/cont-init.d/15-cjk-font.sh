d5db45f11510127e054eefce1c289e28ee92410f
Jocelyn Le Sage <jocelyn@le-sage.com>
2024-11-29 20:39:40 -0500

    Improved compatibility of CJK font installation on Alpine.

--- rootfs/etc/cont-init.d/15-cjk-font.sh
+++ rootfs/etc/cont-init.d/15-cjk-font.sh
@@ -9,8 +9,13 @@ if is-bool-val-true "${ENABLE_CJK_FONT:-0}"; then
         echo "CJK font already installed."
     else
         echo "installing CJK font..."
-        if [ -n "$(which apk)" ]; then
-            add-pkg font-wqy-zenhei --repository "${PACKAGES_MIRROR:-https://dl-cdn.alpinelinux.org/alpine}/edge/community"
+        if command -v apk >/dev/null; then
+            if apk --no-cache info font-wqy-zenhei > /dev/null
+            then
+                add-pkg font-wqy-zenhei
+            else
+                add-pkg font-wqy-zenhei --repository "${PACKAGES_MIRROR:-https://dl-cdn.alpinelinux.org/alpine}/v3.19/community"
+            fi
         else
             add-pkg fonts-wqy-zenhei
         fi
