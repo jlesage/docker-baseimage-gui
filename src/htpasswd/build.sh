f9304579d8bc81b8620d038697c24333b2ccadb2
Jocelyn Le Sage <jocelyn@le-sage.com>
2025-01-26 20:53:09 -0500

    Updated httpd to version 2.4.63.

--- src/htpasswd/build.sh
+++ src/htpasswd/build.sh
@@ -11,7 +11,7 @@ set -u # Treat unset variables as an error.
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Define software versions.
-HTTPD_VERSION=2.4.62
+HTTPD_VERSION=2.4.63
 
 # Define software download URLs.
 HTTPD_URL=https://dlcdn.apache.org/httpd/httpd-${HTTPD_VERSION}.tar.gz
